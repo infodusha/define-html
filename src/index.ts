36c3a2233fc22b5aec26c476fac398756b312fcb
infodusha <infodusha@ya.ru>
2024-07-28 21:26:12 +0300

    refactor: structure index file

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,46 @@ import {
 
 const parser = new DOMParser();
 
-document.addEventListener("DOMContentLoaded", () => {
-	fetchFromLinks().catch(console.error);
-	getFromComments();
+type ComponentMeta = [string, string];
+
+document.addEventListener("DOMContentLoaded", async () => {
+	try {
+		const components = await getComponents();
+		for (const [text, href] of components) {
+			const definedElement = parser.parseFromString(text, "text/html");
+			customElements.define(...createComponent(definedElement, href));
+		}
+	} catch (err) {
+		console.error(err);
+	}
 });
 
-async function fetchFromLinks(): Promise<void> {
-	const links = Array.from<HTMLLinkElement>(
-		document.querySelectorAll(componentSelector)
-	);
-	await Promise.all(links.map(defineHtml));
-}
-
-async function defineHtml(link: HTMLLinkElement): Promise<void> {
+async function fetchFromLink(link: HTMLLinkElement): Promise<ComponentMeta> {
 	const href = returnIfDefined(link.getAttribute("href"));
 	const response = await fetch(href).catch((err) => {
 		throw new Error(`Unable to load ${href}`, { cause: err });
 	});
 	const text = await response.text();
-	const definedElement = parser.parseFromString(text, "text/html");
-	customElements.define(...createComponent(definedElement, href));
+	return [text, href];
 }
 
-function getFromComments() {
-	for (const node of document.head.childNodes) {
-		if (node.nodeType !== Node.COMMENT_NODE) {
-			continue;
-		}
-		if (!node.textContent?.startsWith(commentMarker)) {
-			continue;
-		}
-		const [href, text] = node.textContent
-			.replace(commentMarker, "")
-			.split(/\n(.*)/s);
-		if (!text || !href) {
-			continue;
-		}
+async function getComponents(): Promise<ComponentMeta[]> {
+	const links = Array.from<HTMLLinkElement>(
+		document.querySelectorAll(componentSelector)
+	);
+	const components = await Promise.all(links.map(fetchFromLink));
+	components.push(...getFromComments());
+	return components;
+}
 
-		const definedElement = parser.parseFromString(text, "text/html");
-		customElements.define(...createComponent(definedElement, href));
-	}
+function getFromComments(): ComponentMeta[] {
+	return Array.from(document.head.childNodes)
+		.filter((node) => node.nodeType !== Node.COMMENT_NODE)
+		.filter((node) => !node.textContent?.startsWith(commentMarker))
+		.map((node) =>
+			returnIfDefined(node.textContent)
+				.replace(commentMarker, "")
+				.split(/\n(.*)/s)
+		)
+		.map(([href, text]) => [returnIfDefined(text), returnIfDefined(href)]);
 }
